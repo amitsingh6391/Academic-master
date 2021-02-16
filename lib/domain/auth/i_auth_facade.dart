76f2c7ed6e700478ecdf04cf029f415aa138b6a4
amitsingh6391 <amitsingh506142@gmail.com>
2021-02-17 03:08:09 +0530

    sing-in-block

---
+++ lib/domain/auth/i_auth_facade.dart
@@ -0,0 +1,15 @@
+import 'package:Academicmaster/domain/auth/auth_failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:meta/meta.dart';
+
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
